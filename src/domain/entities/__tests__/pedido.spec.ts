4731327b6a69b438195eb3988ade15515dbe27c6
Anderson Vieira <asvieira.dev@gmail.com>
2025-09-21 17:45:16 -0300

    test(pedido): adiciona teste criar um pedido valido

---
+++ src/domain/entities/__tests__/pedido.spec.ts
@@ -0,0 +1,17 @@
+import { Pedido } from '../pedido';
+import { Produto } from '../produto';
+
+describe('Pedido', () => {
+  let produtos: Produto[];
+
+  beforeEach(() => {
+    produtos = [Produto.criar('p1', 10, 10, 10), Produto.criar('p2', 5, 5, 5)];
+  });
+
+  it('deve criar um pedido válido', () => {
+    const pedido = new Pedido('pedido1', produtos);
+
+    expect(pedido.id).toBe('pedido1');
+    expect(pedido.listaProdutos).toHaveLength(2);
+  });
+});
