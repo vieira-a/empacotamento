afc747a35093f668521a04720f3441a0c143a347
Anderson Vieira <asvieira.dev@gmail.com>
2025-09-21 17:39:55 -0300

    test(produto): adiciona teste para lancar erro caso a altra do produto seja 0 ou negativa

--- src/domain/entities/__tests__/produto.spec.ts
+++ src/domain/entities/__tests__/produto.spec.ts
@@ -8,5 +8,14 @@ describe('Entidade Produto', () => {
       expect(produto.id).toBe('p1');
       expect(produto.dimensoes).toEqual([10, 20, 30]);
     });
+
+    it('deve lançar erro se altura for 0 ou negativa', () => {
+      expect(() => Produto.criar('p2', 0, 10, 10)).toThrow(
+        'Produto p2 com dimensões inválidas.',
+      );
+      expect(() => Produto.criar('p3', -1, 10, 10)).toThrow(
+        'Produto p3 com dimensões inválidas.',
+      );
+    });
   });
 });
