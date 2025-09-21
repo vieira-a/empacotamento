238ab16a4798a213b2efde114f6efa23ce52ce89
Anderson Vieira <asvieira.dev@gmail.com>
2025-09-21 17:26:19 -0300

    feat(docs): adiciona suporte ao swagger

--- src/api/http/rest/controllers/pedido.controller.ts
+++ src/api/http/rest/controllers/pedido.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { EmpacotarPedidosDTO } from 'src/application/dtos/empacotar-pedido.dto';
+import { PedidoDTO } from 'src/application/dtos/pedido.dto';
 import { EmpacotarPedidoUseCase } from 'src/application/use-cases/empacotar-pedido.usecase';
 
 @Controller('pedidos')
@@ -12,7 +13,7 @@ export class PedidoController {
   @ApiOperation({ summary: 'Empacota os pedidos informados' })
   @ApiResponse({ status: 201, description: 'Pedidos empacotados com sucesso' })
   @ApiResponse({ status: 400, description: 'Erro de validação' })
-  @ApiBody({ type: Object })
+  @ApiBody({ type: PedidoDTO })
   empacotar(@Body() body: EmpacotarPedidosDTO) {
     const resultados = body.pedidos.map((pedidoDTO) =>
       this.empacotarPedidoUseCase.execute(pedidoDTO),
