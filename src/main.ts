33dc22ef0081049acbf3ae0643bc8409e004eef3
Anderson Vieira <asvieira.dev@gmail.com>
2025-09-22 09:19:18 -0300

    refactor(bootstrap): muda prefixo global e endpoint para o swagger

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
     }),
   );
 
+  app.setGlobalPrefix('api/v1');
+
   const config = new DocumentBuilder()
     .setTitle('API de Pedidos')
     .setDescription('API para empacotamento de pedidos')
@@ -32,7 +34,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
